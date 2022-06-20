7fb0775c4a9ca84f40b1926e5edfd2345f30549b
coinsccg <59504432+coinsccg@users.noreply.github.com>
2022-06-20 10:01:38 +0800

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,66 +1,63 @@
 require("@nomiclabs/hardhat-waffle");
-require('@nomiclabs/hardhat-ethers');
-require("@nomiclabs/hardhat-etherscan");
-const { mnemonic, BSCSCANAPIKEY} = require('./secrets.json');
+require("@nomiclabs/hardhat-etherscan"); // 合约验证
+require('dotenv').config() // 解析.env
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async () => {
-  const accounts = await ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
-module.exports = {
-  defaultNetwork: "mainnet",
+ module.exports = {
+  defaultNetwork: "rinkeby",
   networks: {
-    localhost: {
-      url: "http://127.0.0.1:8545"
-    },
     hardhat: {
     },
-    testnet: {
-      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
-      chainId: 97,
-      gasPrice: 20000000000,
-      accounts: {mnemonic: mnemonic}
+    rinkeby: {
+      url: "https://rinkeby.infura.io/v3/457c1ac43c544b05abfef0163084a7a6",
+      accounts: [process.env.PRIVKEY]
     },
-    mainnet: {
-      url: "https://bsc-dataseed.binance.org/",
-      chainId: 56,
-      gasPrice: 20000000000,
-      accounts: {mnemonic: mnemonic}
+    ropsten: {
+      url: "https://ropsten.infura.io/v3/457c1ac43c544b05abfef0163084a7a6",
+      accounts: [process.env.PRIVKEY]
+    },
+    bscTest: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+      accounts: [process.env.PRIVKEY]
+    },
+    hecoTest: { // 验证失败
+      url: "https://http-testnet.hecochain.com/",
+      accounts: [process.env.PRIVKEY]
     }
   },
+  
   etherscan: {
-    // Your API key for Etherscan
-    // Obtain one at https://etherscan.io/
-    apiKey: BSCSCANAPIKEY
+    apiKey: {
+      rinkeby: process.env.RINKEBY_APIKEY,
+      ropsten: process.env.RINKEBY_APIKEY,
+      bscTest: process.env.BSCTEST_APIKEY,
+      hecoTest: process.env.HECOTEST_APIKEY
+    },
+    customChains: [
+      {
+        network: "bscTest",
+        chainId: 97,
+        urls: {
+          apiURL: "https://api-testnet.bscscan.com/api",
+          browserURL: "https://testnet.bscscan.com/"
+        }
+      },
+      {
+        network: "hecoTest",
+        chainId: 256,
+        urls: {
+          apiURL: "https://api-testnet.hecoinfo.com/api",
+          browserURL: "https://testnet.hecoinfo.com"
+        }
+      }
+    ]
   },
   solidity: {
-  version: "0.8.0",
-  settings: {
-    optimizer: {
-      enabled: true,
-      runs: 200
-    },
-   }
-  },
-  paths: {
-    sources: "./contracts",
-    tests: "./test",
-    cache: "./cache",
-    artifacts: "./artifacts"
-  },
-  mocha: {
-    timeout: 20000
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
   }
-};
+}
